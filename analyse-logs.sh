cbba2da58b014d71c8f51b82e66999b683f7dde0
Archbaer <pereirakebaili@gmail.com>
2025-11-29 23:57:55 +0000

    adding output files, writing operations

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+echo -e "Analyzing logs...\n"
 
-LOG="application.log"
-ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL" "WARNING")
+LOG_DIR="./logs"
+ERROR_PATTERNS=("INFO" "WARN" "ERROR")
+REPORT_OUT="./report.log"
 
-echo "Starting log analysis..."
-echo "======================="
+if [ ! -d "$LOG_DIR" ]; then
+    echo "Log directory not found!"
+    exit 1
+fi
 
-find . -name "*.log" -mtime -1
+LF=$(find $LOG_DIR -name "*.log")
 
-echo -e "\n Line 1 \n Line 2"
-grep "${ERROR_PATTERNS[0]}" application.log
+echo -e "\nLog files in the ${LOG_DIR} directory:"
+echo "$LF"
+echo "Report created at $(date '+%F %T')" >> $REPORT_OUT
+echo -e "Log files in the ${LOG_DIR} directory: \n $LF" >> $REPORT_OUT
 
-grep -c "${ERROR_PATTERNS[2]}" application.log
-
-grep -c "${ERROR_PATTERNS[1]}" application.log
+for file in $LF; do
+    echo -e "\nAnalyzing file: $file"
+    for pattern in "${ERROR_PATTERNS[@]}"; do
+        COUNT=$(grep -c "$pattern" "$file")
+        echo "Number of '$pattern' entries: $COUNT in $file" >> $REPORT_OUT
+    done
+done
