009f2138faf92a79191c93bdeeeb893fe64cfb93
Archbaer <pereirakebaili@gmail.com>
2025-11-30 13:45:06 +0000

    refactored log analysis output and add .gitignore for report.log

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo -e "Analyzing logs...\n"
-
 LOG_DIR="./logs"
 ERROR_PATTERNS=("INFO" "WARN" "ERROR")
 REPORT_OUT="./report.log"
@@ -11,7 +9,8 @@ if [ ! -d "$LOG_DIR" ]; then
 fi
 
 LF=$(find $LOG_DIR -name "*.log")
-
+echo "===============================" >> $REPORT_OUT
+echo "Log Analysis Report" >> $REPORT_OUT
 echo "Report created at $(date '+%F %T')" >> $REPORT_OUT
 echo -e "\nLog files in the ${LOG_DIR} directory: \n$LF" >> $REPORT_OUT
 
@@ -20,5 +19,10 @@ for file in $LF; do
     for pattern in "${ERROR_PATTERNS[@]}"; do
         COUNT=$(grep -c "$pattern" "$file")
         echo "Number of '$pattern' entries: $COUNT in $file" >> $REPORT_OUT
+        
     done
+    no_match=$(grep -Evi '\b(INFO|ERROR|WARN)\b' "$file")
+    echo "============================" >> $REPORT_OUT
+    echo "Logs that fall outside the common error logs:" >> $REPORT_OUT
+    echo "$no_match" >> $REPORT_OUT
 done
